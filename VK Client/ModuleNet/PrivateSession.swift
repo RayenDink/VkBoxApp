3c20c0994dad0f64588c8b0bc9d035c6cb5699e8
Rayen <zufirka@icloud.com>
2020-09-25 07:13:27 +0300

    test

--- VK Client/ModuleNet/PrivateSession.swift
+++ VK Client/ModuleNet/PrivateSession.swift
@@ -1,1 +1,21 @@
+//
+//  PrivSession.swift
+//  VkBox
+//
+//  Created by Rayen on 9/25/20.
+//  Copyright © 2020 Rayen D. All rights reserved.
+//
+
+
+import Foundation
+
+class Session {
+    
+    static let shared = Session()
+    
+    var token: String?
+    var userId: Int?
+    
+    private init(){}
+}
 
